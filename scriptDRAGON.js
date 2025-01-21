72378f806b2c25959dfa8d25632c35d80e6ec320
Sondre24sfj <sondre24sfj@gmail.com>
2025-01-21 09:56:43 +0100

    dragon*

--- scriptDRAGON.js
+++ scriptDRAGON.js
@@ -1,7 +1,7 @@
-const titleDRAGON = "";
-const imgDRAGONUrl = "";
-const textDRAGON = "";
-const cardColorDRAGON = "";
+const titleDRAGON = "Bubby the angry Dragon";
+const imgDRAGONUrl = "https://static.wikia.nocookie.net/monster/images/3/3c/68185247-BAAD-4E7C-BA78-C2C72761CBDA.jpeg/revision/latest?cb=20190406202034";
+const textDRAGON = "Angry Dragon";
+const cardColorDRAGON = "coral";
 
 if (titleDRAGON && imgDRAGONUrl && textDRAGON && cardColorDRAGON) {
   document.getElementById("titleDRAGON").textContent = titleDRAGON;
