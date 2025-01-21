42dbfe992926764366b2d8411c8565642bd03b86
Lillian24sfj <lillian24sfj@gmail.com>
2025-01-21 09:58:03 +0100

    Lillian1

--- scriptKRAKEN.js
+++ scriptKRAKEN.js
@@ -2,7 +2,7 @@ const titleKRAKEN = "Mio the seamonster of all";
 const imgKRAKENUrl =
   "./images/KRAKEN/kraken_the_sea_monster_by_artsfuture_dge86gu-pre.jpg";
 const textKRAKEN = "I am the scariest monster of all time";
-const cardColorKRAKEN = "coral";
+const cardColorKRAKEN = "blueviolet";
 
 if (titleKRAKEN && imgKRAKENUrl && textKRAKEN && cardColorKRAKEN) {
   document.getElementById("titleKRAKEN").textContent = titleKRAKEN;
